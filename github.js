f8edd034fb36139f3e073cac517dd2dfeb941a68
Yao Min <ngyaominsg@gmail.com>
2018-06-03 18:03:35 +0800

    More on getuser async

--- github.js
+++ github.js
@@ -10,6 +10,13 @@ class Github {
   }
 
   async getUser(user) {
-    const profileResponse
+    const profileResponse = await fetch
+    (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profileData = await profileResponse.json();
+
+    return {
+      
+    }
   }
 }
