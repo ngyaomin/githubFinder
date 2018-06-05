89f41a7ce249ab52308b09a1981e425f01389aba
Yao Min <ngyaominsg@gmail.com>
2018-06-05 18:25:11 +0800

    Adding repos part 1

--- github.js
+++ github.js
@@ -7,16 +7,23 @@ class Github {
     // but since this is not for production, its ok
     this.client_id = 'ce0e9fa370e5e4281f8e'
     this.client_secret = 'd342e5bc9a62a3e1f5632b46684cb6326bd74c4c'
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch
     (`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch
+    (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
