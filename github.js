2c613b670ba6d4e8217cc82a86ae2b7da89f9570
Yao Min <ngyaominsg@gmail.com>
2018-06-05 19:02:51 +0800

    Adding repos part 2

--- github.js
+++ github.js
@@ -19,7 +19,7 @@ class Github {
     (`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     const profile = await profileResponse.json();
-    const repos = await reposResponse.json();
+    const repos = await repoResponse.json();
 
     return {
       profile,
